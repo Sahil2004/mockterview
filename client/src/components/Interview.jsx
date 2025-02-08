9c00bb96a871a6e769a2738daec602f2d7c2374a
Sahil Garg <contact.sahilgarg@protonmail.com>
2025-02-08 21:36:29 +0530

    fix: ternary operator

--- client/src/components/Interview.jsx
+++ client/src/components/Interview.jsx
@@ -56,11 +56,11 @@ const Interview = () => {
           >
             <div class="max-w-3xl mx-auto p-6 bg-white shadow-md rounded-md">
               <h1 class="text-2xl font-semibold text-center mb-2">
-                {data[rand].id}
+                {data[rand]?.id}
               </h1>
 
               <p class="mt-4 text-gray-700 leading-relaxed">
-                {data[rand].statement}
+                {data[rand]?.statement}
               </p>
 
 
